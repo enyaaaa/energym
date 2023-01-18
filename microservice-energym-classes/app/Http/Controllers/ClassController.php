4d5fcecc4583aeb9fe046f5b1deeadd01c3bdd9a
enyaaaa <enya.tan3009@gmail.com>
2023-01-18 16:12:56 +0800

    update user class controller

--- microservice-energym-classes/app/Http/Controllers/ClassController.php
+++ microservice-energym-classes/app/Http/Controllers/ClassController.php
@@ -53,49 +53,90 @@ class ClassController extends Controller
      */
     public function store(Request $request)
     {
-        $validator  = Validator::make($request->all(), [
-            "classTitle" => 'required',
-            "classType" => 'required',
-            "classLocation" => 'required',
-            "classDate" => 'required',
-            "classTime" => 'required',
-            "classDuration" => 'required',
-            "price" => 'required|regex:/^\d+(\.\d{1,2})?$/',
-        ]);
+        if (auth('sanctum')->check()) {
 
-        if ($validator->fails()) {
+            $instructor_id = auth('sanctum')->user()->id;
+            $classTitle = $request->classTitle;
+            $classImage = $request->input('classImage');
+            $classType = $request->input('classType');
+            $classRoom = $request->input('classRoom');
+            $classDate = $request->input('classDate');
+            $classTime = $request->input('classTime');
+            $classDuration = $request->input('classDuration');
+            $price = $request->input('price');
+            $purpose = $request->input('purpose');
+            $description = $request->input('description');
+            $slots = $request->input('slots');
+
+            $classesCheck = classes::where('id', $instructor_id)->first();
+
+            if ($classesCheck) {
+
+                if (classes::where('class_id', $class_id)->where('user_id', $user_id)->exists()) {
+                    return response()->json([
+                        'status' => 409,
+                        'message' => 'you have already booked a slot',
+                    ]);
+                } else {
+                    $validator  = Validator::make($request->all(), [
+                        "classTitle" => 'required',
+                        "classType" => 'required',
+                        "classLocation" => 'required',
+                        "classDate" => 'required',
+                        "classTime" => 'required',
+                        "classDuration" => 'required',
+                        "price" => 'required|regex:/^\d+(\.\d{1,2})?$/',
+                    ]);
+
+                    if ($validator->fails()) {
+                        return response()->json([
+                            'status' => 404,
+                            'message' => 'Some fields are missing',
+                            'validation_errors' => $validator->errors(),
+                        ]);
+                    } else {
+                        $class = new classes();
+
+                        $class->instructor_id = $request->input('instructor_id');
+                        $class->classTitle = $request->input('classTitle');
+                        $class->classType = $request->input('classType');
+                        $class->classDate = $request->input('classDate');
+                        $class->classTime = $request->input('classTime');
+                        $class->classDuration = $request->input('classDuration');
+                        $class->price = $request->input('price');
+                        $class->description = $request->input('description');
+                        $class->slots = $request->input('slots');
+
+                        $file = $request->file('classPic');
+                        $filename = $file->getClientOriginalName();
+                        $file = $request->profilePic->storeAs('classPic/' . $class->classTitle, $filename, "s3");
+                        Storage::disk('s3')->setVisibility($file, 'public');
+                        $url = Storage::disk('s3')->url($file);
+
+                        $class->classImage = $url;
+
+                        $class->save();
+                        return response()->json([
+                            'status' => 200,
+                            'message' => 'Class Added Successfully',
+                        ]);
+                    }
+                }
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Booking not found',
+                ]);
+            }
+
             return response()->json([
-                'status' => 404,
-                'message' => 'Some fields are missing',
-                'validation_errors' => $validator->errors(),
+                'status' => 201,
+                'message' => 'Booked',
             ]);
         } else {
-            $class = new classes();
-
-            $class->instructor_id = $request->input('instructor_id');
-            $class->classTitle = $request->input('classTitle');
-
-            $class->classType = $request->input('classType');
-            $class->classLocation = $request->input('classLocation');
-            $class->classDate = $request->input('classDate');
-            $class->classTime = $request->input('classTime');
-            $class->classDuration = $request->input('classDuration');
-            $class->price = $request->input('price');
-            $class->description = $request->input('description');
-            $class->slots = $request->input('slots');
-
-            $file = $request->file('classPic');
-            $filename = $file->getClientOriginalName();
-            $file = $request->profilePic->storeAs('classPic/' . $class->classTitle, $filename, "s3");
-            Storage::disk('s3')->setVisibility($file, 'public');
-            $url = Storage::disk('s3')->url($file);
-
-            $class->classImage = $url;
-
-            $class->save();
             return response()->json([
-                'status' => 200,
-                'message' => 'Class Added Successfully',
+                'status' => 401,
+                'message' => 'Login to add a booking',
             ]);
         }
     }
