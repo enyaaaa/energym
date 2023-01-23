eecc523c2cb8f56281d6bc64a1f7f5415232e9c5
enyaaaa <enya.tan3009@gmail.com>
2023-01-23 16:19:05 +0800

    created routes for forum controller

--- microservice-energym-forum/routes/api.php
+++ microservice-energym-forum/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ForumController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('forum', ForumController::class);
