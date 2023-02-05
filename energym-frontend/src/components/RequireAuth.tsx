71d582328935dfac97b69418044abd86b92ca5e3
enyaaaa <enya.tan3009@gmail.com>
2023-02-06 03:35:17 +0800

    fix require auth error

--- energym-frontend/src/components/RequireAuth.tsx
+++ energym-frontend/src/components/RequireAuth.tsx
@@ -1,19 +1,16 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
 
-const RequireAuth = () => {
-  //using user login to verify users if they are admin or normal user
-  const { auth }: any = useAuth();
+// const RequireAuth = () => {
 
-  const location = useLocation();
+//   const location = useLocation();
 
-  return auth?.token ? (
-    <Navigate to="/" state={{ from: location }} replace />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+//   return auth?.token ? (
+//     <Navigate to="/" state={{ from: location }} replace />
+//   ) : (
+//     <Navigate to="/login" state={{ from: location }} replace />
+//   );
 
-  /* auth?.token ? <Outlet/> : <Navigate to='/login' state={{ from: location }} replace /> */
-};
+//   /* auth?.token ? <Outlet/> : <Navigate to='/login' state={{ from: location }} replace /> */
+// };
 
-export default RequireAuth;
+// export default RequireAuth;
