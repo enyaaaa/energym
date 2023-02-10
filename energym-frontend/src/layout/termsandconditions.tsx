7ec87c605d36c451640c24489baafe5939cee3e9
enyaaaa <enya.tan3009@gmail.com>
2023-02-10 22:30:21 +0800

    edited layout

--- energym-frontend/src/layout/termsandconditions.tsx
+++ energym-frontend/src/layout/termsandconditions.tsx
@@ -145,18 +145,24 @@ const termsandconditions = () => {
 const Container = styled.div``;
 
 const Wrapper = styled.div`
-  padding: 30px;
-  background: linear-gradient(rgba(27, 27, 27, 0.562), rgba(19, 19, 19, 0.938)),
-    url("https://img.grouponcdn.com/bynder/bjvw6NoX932ZGJNw5NwRHoB1i2R/bj-2048x1229/v1/c870x524.webp")
-      center;
+  margin: 20px;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.274),
+      rgba(207, 207, 207, 0.815)
+    ),
+    url("https://media.istockphoto.com/photos/3d-abstract-background-with-ultraviolet-neon-lights-empty-frame-picture-id1191719793?b=1&k=20&m=1191719793&s=612x612&w=0&h=OMpsFvr6bZiuBkj0HnFyNNTU401COhP317Q_QSwmVpg=")
+      center no-repeat;
+  background-size: cover;
 `;
 
 const Header = styled.div`
   padding: 50px;
+  ${mobile({ padding: "30px" })}
 `;
 
 const Title = styled.h1`
   text-align: center;
+  text-transform: uppercase;
   font-size: 80px;
   ${mobile({ fontSize: "50px" })}
 `;
